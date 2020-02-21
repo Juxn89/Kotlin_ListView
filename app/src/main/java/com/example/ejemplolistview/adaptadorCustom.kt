a32216d71f4cfa4f8f1d2304d9039eac3760254c
Juan Gómez <gb.jc@outlook.com>
2020-02-21 16:36:14 -0600

    Se crea el adaptador personalizado que se enlazará con el template_frutas

--- app/src/main/java/com/example/ejemplolistview/adaptadorCustom.kt
+++ app/src/main/java/com/example/ejemplolistview/adaptadorCustom.kt
@@ -1,4 +1,61 @@
 package com.example.ejemplolistview
 
-class adaptadorCustom {
+import android.content.Context
+import android.media.Image
+import android.view.LayoutInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import org.w3c.dom.Text
+
+class adaptadorCustom(var context:Context, items: ArrayList<Fruta>):BaseAdapter() {
+    var items:ArrayList<Fruta>? = null
+    init {
+        this.items = items
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        var holder:ViewHolder? = null
+        var vista:View? = convertView
+
+        if (vista == null){
+            vista = LayoutInflater.from(context).inflate(R.layout.template_frutas, null)
+            holder = ViewHolder(vista)
+            vista.tag = holder
+        }
+        else {
+            holder = vista.tag as? ViewHolder
+        }
+
+        val item = getItem(position) as Fruta
+        holder?.nombre?.text = item.nombre
+        holder?.image?.setImageResource(item.imagen)
+
+        return vista!!
+    }
+
+    override fun getItem(position: Int): Any {
+        return items?.get(position)!!
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getCount(): Int {
+        return items?.count()!!
+    }
+
+    private class ViewHolder(vista:View) {
+        var nombre:TextView? = null
+        var image:ImageView? = null
+
+        init {
+            this.nombre = vista.findViewById(R.id.tvNombre)
+            this.image = vista.findViewById(R.id.iImagen)
+        }
+    }
 }
