d2393056ed0239dfa388b247193032629b65e3b2
Juan Gómez <gb.jc@outlook.com>
2020-02-21 16:32:32 -0600

    Se agrega la clase para poder manejar la estructura de la data

--- app/src/main/java/com/example/ejemplolistview/Fruta.kt
+++ app/src/main/java/com/example/ejemplolistview/Fruta.kt
@@ -1,4 +1,13 @@
 package com.example.ejemplolistview
 
-class Fruta {
+import android.media.Image
+
+class Fruta (nombre:String, imagen:Int){
+    var nombre:String = ""
+    var imagen:Int = 0
+
+    init {
+        this.nombre = nombre
+        this.imagen = imagen
+    }
 }
